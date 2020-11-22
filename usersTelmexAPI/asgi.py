a96f7b7809439dc3d733bc226b26dc82a8076374
juanpabs <frbeju0@gmail.com>
2020-11-21 19:13:02 -0600

    Change name of the API and create the users app

--- usersTelmexAPI/asgi.py
+++ usersTelmexAPI/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for usersTelmex project.
+ASGI config for usersTelmexAPI project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usersTelmex.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usersTelmexAPI.settings')
 
 application = get_asgi_application()
--- usersTelmexAPI/wsgi.py
+++ usersTelmexAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for usersTelmex project.
+WSGI config for usersTelmexAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usersTelmex.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usersTelmexAPI.settings')
 
 application = get_wsgi_application()
